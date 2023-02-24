e205b2dd21c46ce4e25013b4ee0553f05d952f78
Gen <genesishawkeye42@gmail.com>
2023-02-24 17:45:16 -0300

    created the result screen component

---
+++ src/app/screens/result-screen/result-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-screen',
+  templateUrl: './result-screen.component.html',
+  styleUrls: ['./result-screen.component.css']
+})
+export class ResultScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
