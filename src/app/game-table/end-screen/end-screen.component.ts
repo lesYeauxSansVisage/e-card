749c1fea48ff823a7612905fd96aa7d74d9239c4
Gen <genesishawkeye42@gmail.com>
2023-06-19 19:05:36 -0300

    FEAT: Added an input for result

--- src/app/game-table/end-screen/end-screen.component.ts
+++ src/app/game-table/end-screen/end-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-end-screen',
@@ -7,6 +7,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class EndScreenComponent implements OnInit {
   @Output() restartGame = new EventEmitter<void>();
+  @Input() result: string;
 
   constructor() {}
 
