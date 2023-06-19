1f8b635dcabcc8c9d461afa3693b55e8dec35628
Gen <genesishawkeye42@gmail.com>
2023-06-19 19:05:20 -0300

    FEAT: Added tests for game logic service

--- src/app/services/game-logic.service.spec.ts
+++ src/app/services/game-logic.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { GameLogicService } from './game-logic.service';
 
-describe('GameLogicService', () => {
+fdescribe('GameLogicService', () => {
   let service: GameLogicService;
 
   beforeEach(() => {
@@ -10,7 +10,25 @@ describe('GameLogicService', () => {
     service = TestBed.inject(GameLogicService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should not increase the turn if greater than 12', () => {
+    service.turn = 12;
+    service.increaseTurn();
+
+    expect(service.turn).toEqual(12);
+  });
+
+  it('should call next in isGameOver with truthy value when current turn is 12 and increaseTurn method is called', (done) => {
+    service.turn = 12;
+
+    let called = false;
+
+    service.isGameOver.subscribe((isOver) => {
+      called = true;
+      expect(isOver).toBeTruthy();
+      expect(called).toBeTrue();
+      done();
+    });
+
+    service.increaseTurn();
   });
 });
