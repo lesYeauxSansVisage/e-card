3c4b16428d1e07f87fdf13522a49d7376a83eccb
Gen <genesishawkeye42@gmail.com>
2023-06-26 18:28:57 -0300

    TEST: Added tests for getSlaveDeck method

--- src/app/services/deck.service.spec.ts
+++ src/app/services/deck.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { DeckService } from './deck.service';
 
-describe('DeckService', () => {
+fdescribe('DeckService', () => {
   let service: DeckService;
 
   beforeEach(() => {
@@ -10,7 +10,21 @@ describe('DeckService', () => {
     service = TestBed.inject(DeckService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('getSlaveDeck', () => {
+    it('getSlaveDeck should return a deck with 5 cards', () => {
+      const slaveDeck = service.getSlaveDeck();
+
+      expect(slaveDeck.length).toBe(5);
+    });
+
+    it('getSlaveDeck should return a deck with 4 citizen cards and a slave card', () => {
+      const slaveDeck = service.getSlaveDeck();
+
+      const citizenCards = slaveDeck.filter((card) => card.name === 'citizen');
+      const slaveCard = slaveDeck.find((card) => card.name === 'slave');
+
+      expect(citizenCards.length).toBe(4);
+      expect(slaveCard).toBeTruthy();
+    });
   });
 });
