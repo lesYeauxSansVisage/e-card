197a617ff916ded63f34d248974c8d8c7e095473
Gen <genesishawkeye42@gmail.com>
2023-06-29 19:12:40 -0300

    FEAT: Added necessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './game-table/deck/card/card.component';
+import { ComputerCardComponent } from './game-table/deck/computer-card/computer-card.component';
+import { GameTableComponent } from './game-table/game-table.component';
+import { DeckComponent } from './game-table/deck/deck.component';
+import { ScoreboardComponent } from './game-table/scoreboard/scoreboard.component';
+import { EndScreenComponent } from './game-table/end-screen/end-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ComputerCardComponent,
+    GameTableComponent,
+    DeckComponent,
+    ScoreboardComponent,
+    EndScreenComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
