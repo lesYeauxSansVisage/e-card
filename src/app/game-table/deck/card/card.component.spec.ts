8841767ee0177f622de2da5886c2ce70fb3a4457
Gen <genesishawkeye42@gmail.com>
2023-07-01 16:04:27 -0300

    TEST: Added tests for the card component

--- src/app/game-table/deck/card/card.component.spec.ts
+++ src/app/game-table/deck/card/card.component.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DebugElement } from '@angular/core';
 import { CardComponent } from './card.component';
+import { slaveDeck } from 'src/assets/tests-mock-data';
+import { By } from '@angular/platform-browser';
 
 describe('CardComponent', () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
+  let el: DebugElement;
+
+  const card = slaveDeck.find((card) => card.name === 'slave')!;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
-    })
-    .compileComponents();
+      declarations: [CardComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
+    component.cardData = card;
+    el = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an image src and alt with a filename containining the card name', () => {
+    const image = el.query(By.css('.card__image')).nativeElement;
+
+    expect(image.src.toLowerCase()).toContain(card.name);
+    expect(image.alt).toContain(card.name);
+  });
+
+  it('should have the selected class if cardId is equal to the current selected card number', () => {
+    component.currentActiveCard = card.id;
+    fixture.detectChanges();
+
+    const cardEl = el.query(By.css('.card')).nativeElement;
+
+    expect(cardEl).toHaveClass('selected');
   });
 });
