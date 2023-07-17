a7e0f08ba3a33f259dac4e4769f9f778703811b5
Gen <genesishawkeye42@gmail.com>
2023-07-17 16:33:19 -0300

    FEAT: Added export to deckType

--- src/app/services/game-logic.service.ts
+++ src/app/services/game-logic.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import ICard from '../interfaces/ICard';
 import { BehaviorSubject, Subject } from 'rxjs';
 
-type deckType = 'slave' | 'emperor';
+export type deckType = 'slave' | 'emperor';
 
 @Injectable({
   providedIn: 'root',
